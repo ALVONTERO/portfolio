1d2cf7c3da12dedbb36716c88c584a33b6f15e95
ALVONTERO <83576414+ALVONTERO@users.noreply.github.com>
2023-02-07 15:57:08 +0200

    Finished Navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,44 +1,51 @@
 import React from "react";
-import {
-  Nav,
-  Navbar,
-  Container,
-  NavDropdown,
-  Form,
-  Button,
-} from "react-bootstrap";
+import { Nav, Navbar, Container, Button } from "react-bootstrap";
+import "./Navbar.css";
+import brandImage from "./assets/images/hotpoint.png";
+import SearchIcon from "@mui/icons-material/Search";
 
 function NavBar() {
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar fixed="top" variant="dark" expand="md">
       <Container>
+        <Navbar.Brand href="#">
+          <img
+            src={brandImage}
+            width="80"
+            height="50"
+            className="d-inline-block align-top"
+            alt="brand logo"
+          />
+        </Navbar.Brand>
         <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+          <Nav className="ms-auto mb-2 mb-lg-0" id="main">
+            <Nav.Link className="p-2 p-lg-3 active" href="#home">
+              Home
+            </Nav.Link>
+            <Nav.Link className="p-2 p-lg-3" href="#services">
+              Services
+            </Nav.Link>
+            <Nav.Link className="p-2 p-lg-3" href="#portfolio">
+              Portfolio
+            </Nav.Link>
+            <Nav.Link className="p-2 p-lg-3" href="#about">
+              About
+            </Nav.Link>
+            <Nav.Link className="p-2 p-lg-3" href="#contact">
+              Contact
+            </Nav.Link>
           </Nav>
-          <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-success">Search</Button>
-          </Form>
+          <SearchIcon 
+          fontSize="large"
+            className="search ps-2 mb-2 mt-2 ms-1 me-1 d-none d-lg-block"
+            sx={{
+              color: "#33d1cc",
+              borderLeft: "1px solid #33d1cc",
+            }}
+          />
+          <Button className="btn-primary rounded-pill">Login</Button>
         </Navbar.Collapse>
       </Container>
     </Navbar>

cd2afd5ffd48f5c763bdc7329c31dd1703df1fab
ALVONTERO <83576414+ALVONTERO@users.noreply.github.com>
2023-02-08 18:32:05 +0200

    Created work section

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,8 +1,19 @@
 import React from "react";
-import { Nav, Navbar, Container, Button } from "react-bootstrap";
+import { Nav, Navbar, Container, Button, Row } from "react-bootstrap";
 import "./Navbar.css";
 import brandImage from "./assets/images/vistaz.jpg";
 import SearchIcon from "@mui/icons-material/Search";
+import ScatterPlotIcon from "@mui/icons-material/ScatterPlot";
+import EditIcon from "@mui/icons-material/Edit";
+import { CardContent, Card, CardMedia, Typography } from "@mui/material";
+import MonitorIcon from "@mui/icons-material/Monitor";
+import PowerIcon from "@mui/icons-material/Power";
+import html from "./assets/images/html.jpg";
+import css from "./assets/images/css.jpg";
+import js from "./assets/images/js.png";
+import bootstrap from "./assets/images/bootstrap.png";
+import react from "./assets/images/react.jpg";
+import mui from "./assets/images/mui.jfif";
 
 function NavBar() {
   return (
@@ -45,11 +56,8 @@ function NavBar() {
                 borderLeft: "1px solid #33d1cc",
               }}
             />
-            
-              <Button className="login-btn rounded-pill">
-                Login
-              </Button>
-            
+
+            <Button className="login-btn rounded-pill">Login</Button>
           </Navbar.Collapse>
         </Container>
       </Navbar>
@@ -63,6 +71,273 @@ function NavBar() {
             Get Started
           </Button>
         </div>
+      </div>
+      <div className="features text-center pt-5 pb-5">
+        <Container>
+          <div className="main-title mt-5 mb-5 position-relative">
+            <ScatterPlotIcon
+              className="mb-4"
+              sx={{ fontSize: "5rem", color: "#ffc400" }}
+            />
+            <h2>We are Good at</h2>
+            <p className="text-black-50 text-uppercase">
+              Some of These Stuff Below
+            </p>
+          </div>
+          <Row className="justify-content-around align-items-centers">
+            <Card style={{ maxWidth: 330 }} className="mt-5 col-md-6 col-lg-4">
+              <CardContent className="card-content">
+                <Typography className="typography-icon" component="div">
+                  <EditIcon className="icon mb-3 " />
+                </Typography>
+                <Typography
+                  className="sh-title"
+                  gutterBottom
+                  variant="h5"
+                  component="div"
+                >
+                  Graphic Design
+                </Typography>
+                <Typography
+                  variant="body2"
+                  className="text-start text-black-50 text-uppercase lh-base"
+                  color="text.secondary"
+                >
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat.
+                </Typography>
+              </CardContent>
+            </Card>
+            <Card style={{ maxWidth: 330 }} className="mt-5 col-md-6 col-lg-4">
+              <CardContent className="card-content">
+                <Typography className="typography-icon" component="div">
+                  <MonitorIcon className="icon mb-3 " />
+                </Typography>
+                <Typography
+                  className="sh-title"
+                  gutterBottom
+                  variant="h5"
+                  component="div"
+                >
+                  Graphic Design
+                </Typography>
+                <Typography
+                  variant="body2"
+                  className="text-start text-black-50 text-uppercase lh-base"
+                  color="text.secondary"
+                >
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat.
+                </Typography>
+              </CardContent>
+            </Card>
+            <Card style={{ maxWidth: 330 }} className="mt-5 col-md-6 col-lg-4">
+              <CardContent className="card-content">
+                <Typography className="typography-icon" component="div">
+                  <PowerIcon className="icon mb-3 " />
+                </Typography>
+                <Typography
+                  className="sh-title"
+                  gutterBottom
+                  variant="h5"
+                  component="div"
+                >
+                  Graphic Design
+                </Typography>
+                <Typography
+                  variant="body2"
+                  className="text-start text-black-50 text-uppercase lh-base"
+                  color="text.secondary"
+                >
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat.
+                </Typography>
+              </CardContent>
+            </Card>
+          </Row>
+        </Container>
+      </div>
+      <div className="work-section text-center pt-5 pb-5">
+        <Container>
+          <div className="main-title mt-5 mb-5 position-relative">
+            <ScatterPlotIcon
+              className="mb-4"
+              sx={{ fontSize: "5rem", color: "#ffc400" }}
+            />
+            <h2>We Make This</h2>
+            <p className="text-black-50 text-uppercase">Prepare to be Amazed</p>
+          </div>
+          <Row className="justify-content-around align-items-center">
+            <Card
+              className="mt-3 mb-3"
+              sx={{ minHeight: 432.52, border: "none", maxWidth: 345 }}
+            >
+              <CardMedia
+                component="img"
+                alt="Hyper Text Markup Language"
+                height="200"
+                sx={{ objectFit: "contain" }}
+                image={html}
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Hyper Text Markup Language
+                </Typography>
+                <Typography
+                  variant="body2"
+                  className="text-start"
+                  color="text.secondary"
+                >
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat.
+                </Typography>
+              </CardContent>
+            </Card>
+            <Card
+              className="mt-3 mb-3"
+              sx={{ minHeight: 432.52, border: "none", maxWidth: 345 }}
+            >
+              <CardMedia
+                component="img"
+                alt="Cascading Style Sheet"
+                height="200"
+                sx={{ objectFit: "contain" }}
+                image={css}
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Cascading Style Sheet
+                </Typography>
+                <Typography
+                  variant="body2"
+                  className="text-start"
+                  color="text.secondary"
+                >
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat.
+                </Typography>
+              </CardContent>
+            </Card>
+            <Card
+              className="mt-3 mb-3"
+              sx={{ minHeight: 432.52, border: "none", maxWidth: 345 }}
+            >
+              <CardMedia
+                component="img"
+                alt="JavaScript"
+                height="200"
+                sx={{ objectFit: "contain" }}
+                image={js}
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  JavaScript
+                </Typography>
+                <Typography
+                  variant="body2"
+                  className="text-start"
+                  color="text.secondary"
+                >
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat.
+                </Typography>
+              </CardContent>
+            </Card>
+            <Card
+              className="mt-3 mb-3"
+              sx={{ minHeight: 432.52, border: "none", maxWidth: 345 }}
+            >
+              <CardMedia
+                component="img"
+                alt="Bootstrap"
+                height="200"
+                sx={{ objectFit: "contain" }}
+                image={bootstrap}
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Bootstrap
+                </Typography>
+                <Typography
+                  variant="body2"
+                  className="text-start"
+                  color="text.secondary"
+                >
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat.
+                </Typography>
+              </CardContent>
+            </Card>
+            <Card
+              className="mt-3 mb-3"
+              sx={{ minHeight: 432.52, border: "none", maxWidth: 345 }}
+            >
+              <CardMedia
+                component="img"
+                alt="React"
+                height="200"
+                sx={{ objectFit: "contain" }}
+                image={react}
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  React
+                </Typography>
+                <Typography
+                  variant="body2"
+                  className="text-start"
+                  color="text.secondary"
+                >
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat.
+                </Typography>
+              </CardContent>
+            </Card>
+            <Card
+              className="mt-3 mb-3"
+              sx={{ minHeight: 432.52, border: "none", maxWidth: 345 }}
+            >
+              <CardMedia
+                component="img"
+                alt="Material-UI"
+                height="200"
+                sx={{ objectFit: "contain" }}
+                image={mui}
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Material-UI
+                </Typography>
+                <Typography
+                  variant="body2"
+                  className="text-start"
+                  color="text.secondary"
+                >
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat.
+                </Typography>
+              </CardContent>
+            </Card>
+          </Row>
+        </Container>
       </div>
     </>
   );
